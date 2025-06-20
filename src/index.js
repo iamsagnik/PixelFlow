78f813eb202c7c2b2a3c5f895ea05ec39796055b
SAGNIK_MITRA <sagnikmitra008@gmail.com>
2025-06-20 17:53:37 +0530

    connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dotenv').config({path: './.env'});
+
+import dotenv from "dotenv";
+import connectDB from './db/index.js';
+
+dotenv.config({path: './env'});
+
+connectDB();
+
+
+
+
+
+// const app = express();
+
+// ;( async () => {
+//   try{
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.error(error);
+//       throw error;
+//     });
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Listening on port ${process.env.PORT}`);
+//     });
+//   }
+//   catch(error){
+//     console.error("Error while connecting to DB", error);
+//     throw error;
+//   }
+// })()
